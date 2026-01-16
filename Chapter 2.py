c6f96c20c74b45d80809cf65f2abd7f810eb967c
oxohu <ozohu.15@gmail.com>
2026-01-16 13:29:39 +0100

    variables

---
+++ Chapter 2.py
@@ -0,0 +1,9 @@
+#using 4 arithmethic operations to get the value 8
+print(4+4)
+print(2*4)
+print(10-2)
+
+#favourite number saved to a variable, using the f string
+fav_number = 10
+message = f"my favourite number is {fav_number}"
+print(message)
