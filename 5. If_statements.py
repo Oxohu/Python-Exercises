07e87d9b467d1522449a8e701ab3553f7d3d6782
oxohu <ozohu.15@gmail.com>
2026-01-21 21:08:59 +0100

    welcome users

--- 5. If_statements.py
+++ 5. If_statements.py
@@ -7,9 +7,10 @@ print(car == "audi")
 
 def check_score(alien_colour: str):
     """
+    Docstring for stage_of_life
     this function uses an if statement to verify whether or not the
     gamer guessed the correct colour of the alien that was shot down
-    alien_colour  is a variable that stores the gamer's choice of colour
+    :param alien_colour: is a variable that stores the gamer's choice of colour
     """
     if alien_colour == "green":
         print("you just earned 5 points")
@@ -66,3 +67,20 @@ def affinity_fruits(fruit: str):
 fruit = input("what fruits do you want?")
 affinity_fruits(fruit)
 
+def user_greeting(usernames: str):
+    """
+    Docstring for user_greeting
+    
+    :param usernames: Description
+    :type usernames: str
+    """
+    usernames = ["Admin", "habeeb", "nafist", "fatimah","aisha"]
+    for name in usernames:
+        if name == "Admin":
+            print("Hello admin, would you like to see a status report?")
+            break
+        else:
+            print("welcome back user")
+
+name = input("enter your user name: ")
+user_greeting(name)
