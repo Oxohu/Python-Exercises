608c483df75f2003ac9673834f3e9e09de7c49b9
oxohu <ozohu.15@gmail.com>
2026-01-21 02:31:33 +0100

    age stage determinant

--- 5. If statements.py
+++ 5. If statements.py
@@ -14,8 +14,36 @@ def check_score(alien_colour: str):
     '''
     if alien_colour == "green":
         print("you just earned 5 points")
+    elif alien_colour == "yellow":
+           print("you just earned 10 points")
+    elif alien_colour == "red":
+         print("you just earned 15 points")
     else:
-        print("you got the alien colour wrong")
+         print("you got the alien colours wrong")
 
-alien_colour = "green"
+alien_colour = "red"
 check_score(alien_colour)
+
+
+def stage_of_life(age: int):
+    """
+    Docstring for stage_of_life
+    
+    :param age: Description
+    :type age: int
+    """
+    if age < 2:
+        print("you are a child")
+    elif age >= 2 and age < 4:
+        print("you are a toddler")
+    elif age >= 4 and age < 13:
+        print("you are a kid")
+    elif age >= 13 and age < 20:
+        print("you are a teenager")
+    elif age >= 20 and age < 65:
+        print("you are an adult")
+    else:
+        print("you are an elder")
+
+age = int(input("how old are you? "))
+stage_of_life(age)
