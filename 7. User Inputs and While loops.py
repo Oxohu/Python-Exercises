95239a963b0500f85b2395a6d3efb3f684dfdf5e
oxohu <ozohu.15@gmail.com>
2026-01-27 10:38:04 +0100

    multiple of ten

--- 7. User Inputs and While loops.py
+++ 7. User Inputs and While loops.py
@@ -3,12 +3,50 @@ def rental_car() -> str:
     this function prompts the user for input on the type of car they 
     want to rent
 
-    return input on the user's choice
+    :return: message informing the user that the rental car they want will be checked for
     rtype: str
     """
-    car_type = input("what type of rental car would you like?")
+    car_type = input("what type of rental car would you like? ")
     return car_type
 
 car = rental_car()
 print(f"let me see if I can find you a {car}")
+
+def dinner_group() -> str:
+    """
+    this code uses input from the user to decide if there is a table available
+    for them or if they have to  wait a while befor getting a table that can suffice
+    for the amount of people they invited to the dinner party
+    
+    :return: A message indicating table availability.
+    :rtype: str
+    """
+    people_seating = int(input("How many people are in your dinner group? "))
+    if people_seating < 8:
+        return ("there is a table available for you now")
+    elif people_seating > 8:
+        return ("you have to wait a while to get a table")
+    else:
+        return ("you have just the perfect number of people")
+    
+print(dinner_group())
+
+
+def multiple_of_ten() -> bool:
+    """
+    the function takes in an input from the user and confirms if it is
+    a multiple of 10 or not by untilizing the modulus operation
+    
+    :return: boolean confirming if the number is a multiple of 10 or not
+    :rtype: bool
+    """
+    prompt = "You are to provide me with a number and i'll confirm if it \n is a multiple of 10 or not"
+    prompt +="\n Enter Number here"
+    number = int(input(prompt))
+    if number % 10 == 0:
+        return True
+    else:
+        return False
+
+print(multiple_of_ten())
 
